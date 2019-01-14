51b7cfb63962fc3e73681e18657d2ae2bd990dc6
Heitor Teixeira <heitor.teixeira@hotmail.com>
2019-01-14 21:42:30 -0200

    Criando pagina de produtos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -25,9 +25,11 @@ export class CategoriasPage {
       .subscribe(response => {
         this.items = response;
       },
-      error => {});
+      error => {});  
+  }
 
-    
+  showProdutos(){
+    this.navCtrl.push('ProdutosPage');
   }
 
 }
